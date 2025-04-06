e60cd7883b1c2aa197e7524cbebedbd85c9a91f2
Shun Ueda <me@shu.nu>
2025-04-06 11:09:03 -0400

    refactor: add error

--- src/apple/index.ts
+++ src/apple/index.ts
@@ -18,40 +18,54 @@ const params = new URLSearchParams({
 const matchedJobs: MatchedJob[] = []
 
 for (let i = 1, end = false; !end; i++) {
-  serachUrl.search = params.toString()
-  serachUrl.searchParams.set('page', i.toString())
+  try {
+    serachUrl.search = params.toString()
+    serachUrl.searchParams.set('page', i.toString())
 
-  const res = await fetch(serachUrl)
-  const html = await res.text()
-  const jobs = (
-    parseHTML(html)
-      .document.getElementById('search-job-list')
-      ?.querySelectorAll<HTMLAnchorElement>('a') || []
-  )
-    .values()
-    .filter(job => job.href.startsWith('/en-us/details/'))
-    .toArray()
-  for (const job of jobs) {
-    if (!checkKeywords(job.textContent, apple.keywords.title)) {
-      continue
-    }
-    const url = new URL(job.href, baseUrl)
-    const res = await fetch(url)
+    const res = await fetch(serachUrl)
     const html = await res.text()
-    const { textContent } = parseHTML(html).document.body
-    if (!checkKeywords(textContent, apple.keywords.description)) {
-      continue
+    const jobs = (
+      parseHTML(html)
+        .document.getElementById('search-job-list')
+        ?.querySelectorAll<HTMLAnchorElement>('a') || []
+    )
+      .values()
+      .filter(job => job.href.startsWith('/en-us/details/'))
+      .toArray()
+    for (const job of jobs) {
+      try {
+        if (!checkKeywords(job.textContent, apple.keywords.title)) {
+          continue
+        }
+        const url = new URL(job.href, baseUrl)
+        const res = await fetch(url)
+        const html = await res.text()
+        const { textContent } = parseHTML(html).document.body
+        if (!checkKeywords(textContent, apple.keywords.description)) {
+          continue
+        }
+        const id = url.pathname.split('/').at(3)
+        if (!id || apple.checkpoint === id) {
+          end = true
+          break
+        }
+        matchedJobs.push({
+          id,
+          title: job.textContent || '',
+          url
+        })
+      } catch (e) {
+        console.error('Error processing job:', e)
+        console.error(
+          `Job URL: ${job.href}, Job Title: ${job.textContent}, Job ID: ${job.href.split('/').at(3)}`
+        )
+      }
     }
-    const id = url.pathname.split('/').at(3)
-    if (!id || apple.checkpoint === id) {
-      end = true
-      break
-    }
-    matchedJobs.push({
-      id,
-      title: job.textContent || '',
-      url
-    })
+  } catch (e) {
+    console.error('Error fetching jobs:', e)
+    console.error(`Page: ${i}, URL: ${serachUrl.toString()}`)
+    end = true
+    break
   }
 }
 
